0ff83ea1f29ed7fc183d7256ce4215508883a8c2
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-07 22:34:50 -0500

    cleaning player prefab

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Todo : MonoBehaviour
 {
     // CORE GAMEPLAY:
-    // TODO: build bare player prefab
     // TODO: test top-down player movement
     // TODO: create a zombie object to follow the player
     // TODO: create a group prefab of leaders and followers
