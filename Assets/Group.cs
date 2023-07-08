5e2edc0a6baa727989552237833d54ac4d91d0b4
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-07 23:34:40 -0500

    test tile set in scene

---
+++ Assets/Group.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    [SerializeField] private GameObject _leader;
+    [SerializeField] private List<GameObject> _followers = new List<GameObject>();
+
+    
+}
