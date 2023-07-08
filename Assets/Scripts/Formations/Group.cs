04610e0c47e2017441620703e3b811703a78d2b3
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-08 01:00:45 -0500

    renaming examplearmy to group script; assigned to prefab

--- Assets/Scripts/Formations/Group.cs
+++ Assets/Scripts/Formations/Group.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class ExampleArmy : MonoBehaviour {
+public class Group : MonoBehaviour {
     private FormationBase _formation;
 
     public FormationBase Formation 
