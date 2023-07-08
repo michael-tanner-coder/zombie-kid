28ac63b2616dd9ffa3257201cc1cb87e8e63a297
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-07 22:29:21 -0500

    todo list for game jam

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -2,35 +2,50 @@ using UnityEngine;
 
 public class Todo : MonoBehaviour
 {
-    // REFACTOR todos:
-    // FIXME: UI de-coupling
-    // FIXME: tool tips and getters
-    // FIXME: regioning for larger scripts
-    // FIXME: naming conventions
-    // FIXME: removed commented code and unused scripts
-    // FIXME: same folder structure for all domains
-    // FIXME: state machines via animation controller
+    // CORE GAMEPLAY:
+    // TODO: build bare player prefab
+    // TODO: test top-down player movement
+    // TODO: create a zombie object to follow the player
+    // TODO: create a group prefab of leaders and followers
+    // TODO: set up 2d navmesh pathfinding for group prefabs
+    // TODO: assign number strength to groups
+    // TODO: create enemy groups (leader determines behavior)
+    // TODO: use number strength to determine outcome of group collision (bigger group reduces smaller group gradually)
+    // TODO: create a detection radius for enemy groups
+    // TODO: code flee behavior
+    // TODO: code chase behavior
+    // TODO: create game over state (0 zombies)
+    // TODO: retry button (reset game state)
+    // TODO: create enemy spawner (bigger groups over time, more frequent spawns, spawn far away out of camera bounds)
+    // TODO: set up input bindings
+    // TODO: get aim direction from mouse input
+    // TODO: send out zombie with left click (lose a number in your group, spawn a smaller group composed of one zombie)
+    // TODO: recall zombie with right click (spawn a call radius prefab, slowly increase size over time, recall zombie when in radius)
 
-    // FOR POST-RELEASE:
-        // TODO: Resolve camera/UI differences between hub area and dungeon scenes 
-        // TODO: Create a player spawner to spawn the player based on certain flags
+    // GRAPHICS:
+    // TODO: pick target resolution
+    // TODO: pick color palette
+    // TODO: code camera behavior / parameters
+    // TODO: create sprites for player, zombies, and three enemy types, capture bubble, bubble shot
+    // TODO: create background (abstract? city scape???)
+    // TODO: cutscene graphics/writing
 
-        // SHOP:
-        // TODO: equipment functionality
-        // TODO: emit particles from shop chimney
-        // TODO: change particle texture after player buys things
-        // TODO: play cursor sound
+    // SOUND: 
+    // TODO: movement sound
+    // TODO: send sound
+    // TODO: recall sound
+    // TODO: hug sound
+    // TODO: lose zombie sound
+    // TODO: shoot sound
+    // TODO: shoot sound
+    // TODO: combat music
+    // TODO: menu music
+    // TODO: intro/outro music
 
-        // HELL WELL:
-        // TODO: balance difficulty to reward ratio
-        // TODO: update loot drops based on difficulty selection
-        // TODO: make menu appear after talking to hell well
-
-        // SCORE GRAVES:
-        // TODO: show correct grave scores on scene start
-
-        // GENERAL: 
-        // TODO: add flags for all character dialog unlocks
-        // TODO: fix chest functionality
-        // TODO: fix sound/music volume changer
+    // UI:
+    // TODO: start menu
+    // TODO: game over screen
+    // TODO: HUD
+    // TODO: options
+    // TODO: cutscene UI
 }
